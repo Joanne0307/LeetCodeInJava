b4eeaba1020f592f369a20fac4b268ad469a055b
Joanne0307 <c00823ue@gmail.com>
2017-07-27 19:59:50 +0800

    Update HashSetSolution.java

--- Intersection-of-two-linked-Lists/HashSetSolution.java
+++ Intersection-of-two-linked-Lists/HashSetSolution.java
@@ -1,1 +1,61 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */ 
 
+/*
+  Time complexity: O(m+n)
+  Space complexity: O(m) or O(n)
+*/
+ 
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        
+        HashSet<Integer> set = new HashSet<Integer>();        
+        ListNode result = null;
+        
+        if(headA==null||headB==null) return null;
+        
+        
+            do{
+                
+                set.add(headA.val);   
+                headA = headA.next;
+            }
+            while(headA!=null);
+        
+        
+            do{
+                
+                
+                if(set.contains(headB.val))
+                {
+                    if(result == null){
+                         result = new ListNode(headB.val);
+                    }else{
+
+                        result.next = new ListNode(headB.val);
+                    }
+
+                }
+                
+                headB = headB.next;
+
+            }
+            while(headB!=null);
+
+
+       
+        
+        return result;        
+        
+        
+    }
+}
